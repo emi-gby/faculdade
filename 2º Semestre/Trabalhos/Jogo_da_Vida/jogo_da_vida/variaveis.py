62abbaf7a4219b2a7081f7312fe75d54c693a55c
emi-gby <emitrzs.hub@gmail.com>
2025-10-04 15:39:43 -0300

    Deleta diretório vazio

--- 2º Semestre/Trabalhos/Jogo_da_Vida/jogo_da_vida/variaveis.py
+++
@@ -1,6 +0,0 @@
-import json
-
-caminho_person_img = 'jogo_da_vida/imagens/chaves.png'
-
-with open('jogo_da_vida/dados/perg_resp.json') as f:   
-    dados = json.load(f) 
---
+++ 2º Semestre/Trabalhos/Jogo_da_Vida/variaveis.py
@@ -0,0 +1,6 @@
+import json
+
+caminho_person_img = 'imagens/chaves.png'
+
+with open('dados/perg_resp.json') as f:
+    dados = json.load(f) 
