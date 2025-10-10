5450f22dc26661763f4b5a9690e8d363f7f2e221
emi-gby <emitrzs.hub@gmail.com>
2025-10-10 20:54:21 -0300

    Correção da codificação do arquivo json para UTF-8

--- 2-Semestre/Trabalhos/Jogo_da_Vida/variaveis.py
+++ 2-Semestre/Trabalhos/Jogo_da_Vida/variaveis.py
@@ -7,9 +7,9 @@ caminho_botao_img = 'imagens/Button_1.png'
 
 caminho_person_img = 'imagens/professora.png'
 
-caminho_bg_img = 'imagens/Background_escola2.jpg'
+caminho_bg_img = 'imagens/Background_escola.jpg'
 
 caminho_balao_perg = 'imagens/Balao_pergunta.png'
 
-with open('dados/perg_resp.json') as f:   
+with open('dados/perg_resp.json',encoding='UTF-8') as f:   
     dados = json.load(f) 
