e000fb1d9dcc975cdf52de3903e6c4d9c8c65440
emi-gby <emitrzs.hub@gmail.com>
2025-10-17 15:40:42 -0300

    Modificação das imagens e adição da lógica do karma

---
+++ 2-Semestre/Trabalhos/Jogo_da_Vida/cenarios.py
@@ -0,0 +1,136 @@
+import customtkinter as ctk
+from PIL import Image, ImageTk
+import variaveis as var
+from janelas import *
+
+class JogoDaVida(ctk.CTkToplevel):
+    def __init__(self,master,fase,karma):
+        super().__init__(master)
+        self.geometry(f'{var.WIN_LARGURA}x{var.WIN_ALTURA}')
+        self.title(f'{fase.capitalize()}')
+        self.resizable(False,False)
+        self.fase_atual = fase
+        self.karma = karma
+        var.pegar_imagem(fase)
+
+        #Questão atual --> no começo do jogo
+        self.questao_atual = '1'    
+
+        #Cria canvas 
+        self.canvas = ctk.CTkCanvas(self, width=var.WIN_LARGURA, height=var.WIN_ALTURA, highlightthickness=0)
+        self.canvas.pack(fill="both", expand=True)
+
+        #Imagem background 
+        self.background_img = Image.open(var.caminho_bg_img).resize((var.WIN_LARGURA,var.WIN_ALTURA))
+        self.background_photo = ImageTk.PhotoImage(self.background_img)
+        self.canvas.create_image(0,0,image=self.background_photo,anchor='nw')
+
+        #Imagem do balao de pergunta
+        self.balao_img = Image.open(var.caminho_balao_perg).resize((1500,400))
+        self.balao_photo = ImageTk.PhotoImage(self.balao_img)
+        self.canvas.create_image(var.WIN_LARGURA/2, 90, image=self.balao_photo)
+
+        #Display do texto da questão
+        self.canvas.create_text(var.WIN_LARGURA/2,55,font=('consolas',20),text='',tags='texto_questao') 
+
+        #Imagem do botao
+        self.botao_img = Image.open(var.caminho_botao_img).resize((470,230))
+        self.botao_photo = ImageTk.PhotoImage(self.botao_img)
+
+        #Imagem Karma
+        self.karma_img = Image.open(var.caminho_karma_img).resize((70,70))
+        self.karma_photo = ImageTk.PhotoImage(self.karma_img)
+
+        self.update_interface()
+        
+    def update_interface(self):
+        '''Atualiza a interface com os textos das perguntas e das repostas a partir da questão atual'''
+
+        #Se não tiver mais questões o jogo acaba
+        if self.questao_atual is None:
+            self.destroy()
+            return
+
+        #Dados referentes a questão atual
+        self.dados_questao = var.dados['perguntas'][self.fase_atual][self.questao_atual]
+
+        #Definir texto da questao
+        self.canvas.itemconfig('texto_questao', text=self.dados_questao['texto'])
+
+        #Armazena os textos das respostas
+        respostas = list(self.dados_questao['respostas'].keys())  #resposta é uma lista com os textos das repostas e vai ser passada como uma chave porque cada texto é a chave do dicionario de respostas.
+
+        self.criar_botao_img(respostas)  #Chama a função que cria os botões
+
+
+    def criar_botao_img(self,chave):
+        ''' Cria a imagem e o texto dos botões no canvas a partir das funções create_image/create_text'''
+
+        mult_x_coord = [1,3,5]
+        for i, mult in enumerate(mult_x_coord):
+            #cria e posiciona cada imagem e texto do botao e associa cada um a uma tag correspondente
+            self.canvas.create_image(var.WIN_LARGURA/6*mult,630, image=self.botao_photo,tags=f'botao{i}')
+            self.canvas.create_text(var.WIN_LARGURA/6*mult,630,text=chave[i],fill='white',font=('consolas',15),tags=f'botao{i}')
+            
+        #Passa o texto corresponde ao botão para a função clicar_botao e cria um evento de clicar para cada botao de acordo com sua tag
+        self.canvas.tag_bind(f"botao{0}", "<Button-1>", lambda _: self.clicar_botao(chave[0])) 
+        self.canvas.tag_bind(f"botao{1}", "<Button-1>", lambda _: self.clicar_botao(chave[1]))
+        self.canvas.tag_bind(f"botao{2}", "<Button-1>", lambda _: self.clicar_botao(chave[2]))
+
+            
+    def clicar_botao(self,chave):
+        '''Disabilita os botões e define um tempo de espera para a próxima questão'''
+        for i in range(3):
+            self.canvas.tag_unbind(f'botao{i}', "<Button-1>")
+
+        # mostrar karma na tela
+        self.mostrar_karma(self.dados_questao['respostas'][chave]['lv'])
+
+        self.after(1000,lambda: self.processar_clique(chave))
+
+    def mostrar_karma(self, valor):
+        '''Animação da mudança do karma'''
+
+        #adiciona sinal de mais no inicial se valor for mais ou igual a 0
+        text = f"+{valor}" if valor >= 0 else f"{valor}"
+
+        #cria texto e imagens referentes ao karma
+        self.canvas.create_text(
+            1100, 250, text=text,
+            font=('consolas', 70, 'bold'),
+            fill="#66DF66" if valor > 0 else "#F0E9E9" if valor == 0 else "#CA2323",
+            anchor='center',tags='karma'
+        )
+        self.canvas.create_image(1195,250,image=self.karma_photo, tags='karma')
+
+        # parametros para a animação
+        duracao = 1000  # duração total da animação
+        passos = 30  #quantas 'frames' ocorrem na animação | relacionando com fps- 30 frames(passos)/ 1s(duração)
+        tempo_passo = duracao // passos #tempo em que cada passo ocorre
+        delta_y = -28 / passos  # movimento vertical da frame a cada passo (movimento total = 28px)
+
+        def animacao(passo=0):
+            if passo < passos:
+                # move a imagem e o texto para cima
+                self.canvas.move('karma', 0, delta_y)
+
+                # chama a função animacao após passar o tempo do passo
+                self.after(tempo_passo, lambda: animacao(passo + 1))
+            else:
+                # animação acaba - deleta o texto e a imagem
+                self.canvas.delete('karma')
+
+        animacao()
+
+
+    def processar_clique(self,chave):
+        '''Atualiza a questão atual e deleta os botões antigos'''
+        #chave corresponde ao texto da resposta 
+        self.questao_atual = self.dados_questao['respostas'][chave]['prox_q']
+
+        #deleta os botões
+        for i in range(3):
+            self.canvas.delete(f'botao{i}')
+
+        self.update_interface()
+        
--- 2-Semestre/Trabalhos/Jogo_da_Vida/variaveis.py
+++ 2-Semestre/Trabalhos/Jogo_da_Vida/variaveis.py
@@ -3,35 +3,38 @@ import json
 WIN_ALTURA = 750
 WIN_LARGURA = 1350
 
-caminho_botao_img = 'imagens/Button_1.png'
+caminho_botao_img = 'imagens/Botao.png'
+
 caminho_balao_perg = 'imagens/Balao_pergunta.png'
+
+caminho_karma_img = 'imagens/karma.jpeg'
 
 caminho_bg_img = ''
 
 caminho_main_img = 'imagens/backgrounds/main_background.jpg'
 
-caminho_trabalho_img = 'imagens/edificios/trabalho.png'
+caminho_trabalho_img = 'imagens/edificios/trabalho2.png'
 
-caminho_escola_img = 'imagens/edificios/escola.jpeg'
+caminho_escola_img = 'imagens/edificios/escola2.png'
 
-caminho_familia_img = 'imagens/edificios/familia.jpeg'
+caminho_familia_img = 'imagens/edificios/familia2.png'
 
-caminho_faculdade_img = 'imagens/edificios/faculdade.jpg'
+caminho_faculdade_img = 'imagens/edificios/faculdade2.png'
 
 
 def pegar_imagem(background):
-    global caminho_bg_img, caminho_person_img
+    global caminho_bg_img
     if background == 'escola':
-        caminho_bg_img = 'imagens/backgrounds/background_escola.jpg'
+        caminho_bg_img = 'imagens/backgrounds/background_escola2.jpeg'
 
     elif background == 'familia':
-        caminho_bg_img = 'imagens/backgrounds/background_familia.jpg'
+        caminho_bg_img = 'imagens/backgrounds/background_familia.jpeg'
 
     elif background == 'faculdade':
-        caminho_bg_img = 'imagens/backgrounds/background_faculdade.jpg'
+        caminho_bg_img = 'imagens/backgrounds/background_faculdade2.png'
 
     else:
-        caminho_bg_img = 'imagens/backgrounds/background_trabalho.jpg'
+        caminho_bg_img = 'imagens/backgrounds/background_trabalho4.jpeg'
 
     
 
